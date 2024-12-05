e2d084b90ef49217ccf32476fd18d7739f606724
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-12-05 18:39:05 +0100

    start

---
+++ src/components/header/header.jsx
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+return(
+    <header>
+        <div className="header-container">
+            <div className="header-first">
+                <h2 className="header-logo">CRYPTO<span className="header-logo-part">GEEK</span></h2>
+            </div>
+            <div className="header-midle">
+                <ul className="header-list">
+                    <li  className="header-list-item"><a href="">Academy</a></li>
+                    <li className="header-list-item"> <p className="header-list-menu">Knowledge base</p>
+                        <ul className="header-menu">
+                            <li className="header-menu-item">
+                                <div className="header-menu-div"></div>
+                            </li>
+                            <li className="header-menu-item"></li>
+                            <li className="header-menu-item"></li>
+                            <li className="header-menu-item"></li>
+                        </ul>
+                    </li>
+                    <li className="header-list-item"></li>
+                    <li className="header-list-item"></li>
+                </ul>
+            </div>
+            <div className="header-end"></div>
+        </div>
+    </header>
+)
