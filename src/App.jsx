7ca083cc2466f887266e4965ed92e3fe8e5b0ed4
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-12-07 21:24:12 +0100

    header upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 
 import './App.css'
-import { Header } from './components/header/header'
+import { Header } from './components/header/header.jsx'
 
 function App() {
+    return(
  <Header/>
+)
 }
 
-export default App
+export default App;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,15 @@
-
+import { useState } from "react"
+import './header.css'
 
 export const Header = ()=>{
 
+const [isSubMenuVisible, setIsSubMenuVisible] = useState(false);
+const handleMouseEnter = () => setIsSubMenuVisible(true);
+const handleMouseLeave = ()=> setIsSubMenuVisible(false);
 
-
-
+const [isModalOpen, setIsModalOpen]= useState(false);
+const openModal = () => setIsModalOpen(true);
+const closeModal = () => setIsModalOpen(false);
     
 return(
     <header>
@@ -15,8 +20,10 @@ return(
             <div className="header-midle">
                 <ul className="header-list">
                     <li  className="header-list-item"><a href="">Academy</a></li>
-                    <li className="header-list-item"> <p className="header-list-menu">Knowledge base</p>
-                        <ul className="header-menu">
+                    <li className="header-list-item" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}> <p className="header-list-menu">Knowledge base</p>
+                       
+                       {isSubMenuVisible&&(
+                        <ul className={`header-menu ${isSubMenuVisible ? 'visible' : ''}`}>
                             <li className="header-menu-item">
                                 <div className="header-menu-div" href="">
                                     <h4 className="hover-menu-title">THEMES</h4>
@@ -36,27 +43,79 @@ return(
                                     <p className="hover-menu-p">These crypto coins have their own blockchains</p>
                                 </div></li>
                         </ul>
+                    )}
                     </li>
                     <li className="header-list-item">Feed</li>
                     <li className="header-list-item">Tools</li>
                 </ul>
             </div>
             <div className="header-end">
-                <div className="language">
+                {/* <div className="language">
                     <ul className="select-language">
                         <li className="language-choise">EN</li>
                         <li className="language-choise">UK</li>
                         <li className="language-choise">PL</li>
                     </ul>
-                </div>
+                </div> */}
                 <div className="sing-in">
-                    <button className="sing-btn">Sing in</button>
+                    <button onClick={openModal} className="sing-btn">Sing in</button>
                 </div>
             </div>
+            {isModalOpen &&(
             <div className="modal">
-                
+                <div className="header-modal">
+                    <h3 className="modal-title">Sing In</h3>
+                    <button onClick={closeModal}>
+                 <svg width="28px" height="28px" className="modal-close-icon">
+                    <use href="/images/icons.svg#icon-x-mark"/>
+                 </svg>
+                 </button>
+                </div>
+                <div className="modal-form-cnt">
+                    <form className="modal-form">
+                    <input
+                type="email"
+                name="user-email"
+                className="modal-input"
+                id="user-email"
+                placeholder="Please enter your email"
+              />
+              <div className="form-btns">
+                <button className="modal-link">Sent login link</button>
+                <button className="google-modal">
+                    <svg width="15px" height="15px" className="google-login-icon">
+                        <use href="/images/icons.svg#icon-google"/>
+                    </svg>
+                </button>
+              </div>
+                    </form>
+                </div>
+                <div className="wallet-conection-div">
+                    <div className="walet-modal-title">
+                    
+                       <svg width="15px" height="15px" className="wallet-modal-icon">
+                        <use href="/images/icons.svg#icon-wallet"/>
+                        </svg>
+                        <p className="conect-wallet"> OR CONNECT WALLET
+                    </p>
+                    </div>
+                    <div className="wallets-selection">
+                        <button className="metamask-btn">
+                            <svg width="15px" height="15px" className="metamask-icon">
+                                <use href="/images/icons.svg#metamask"/>
+                            </svg> <span className="metamask-p">MetaMask</span>
+                        </button>
+                        <button className="other-wallets-btn">
+                            <svg width="15px" height="15px" className="other-wallets-icon">
+                                <use href="/images/icons.svg#wallets-icon"/>
+                            </svg> <span> Other wallets</span>
+                        </button>
+                    </div>
+                </div>
             </div>
+            )}
         </div>
+        
     </header>
 )
 }
